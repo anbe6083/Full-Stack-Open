1fd786d320102fe76087addeb44e530aed1efb92
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2026-01-22 08:42:29 -0800

    Exercise 1.7

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -7,23 +7,32 @@ const Button = ({handleClick, text}) => {
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState (0);
+  const [total, setTotal] = useState (0);
   const [neutral, setNeutral] = useState (0);
   const [bad, setBad] = useState (0);
   console.log ('Good on render is: ', good);
   console.log ('neutral on render is: ', neutral);
   console.log ('bad on render is: ', bad);
+
   const handleClickGood = () => {
     console.log ('Good before update is: ', good);
     setGood (good + 1);
+    const updatedTotal = total + 1;
+    setTotal (updatedTotal);
   };
   const handleClickNeutral = () => {
     console.log ('Neutral before update is: ', neutral);
     setNeutral (neutral + 1);
+    const updatedTotal = total + 1;
+    setTotal (updatedTotal);
   };
   const handleClickBad = () => {
     console.log ('Bad before update is: ', bad);
     setBad (bad + 1);
+    const updatedTotal = total + 1;
+    setTotal (updatedTotal);
   };
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -34,6 +43,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p> Average {total === 0 ? 0 : (good - bad) / total}</p>
+      <p> Positive {good / total * 100}%</p>
     </div>
   );
 };
