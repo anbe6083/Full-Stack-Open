0ab7a581f55aaa1637ceaa03c10eef9530aacada
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2026-01-28 11:19:32 -0800

    Setup for part 3

--- part2-notes-frontend/index.js
+++ part2-notes-frontend/index.js
@@ -72,7 +72,7 @@ app.post("/api/notes", (req, res) => {
   res.json(note);
 });
 
-const PORT = 3002;
+const PORT = 3001;
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
 function generateId() {
