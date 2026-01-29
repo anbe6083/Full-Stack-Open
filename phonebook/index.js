28e455988e023b089cd27a2af6a571e1b178282f
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2026-01-29 09:50:50 -0800

    Add environment variable port

--- phonebook/index.js
+++ phonebook/index.js
@@ -90,6 +90,6 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT);
 console.log("Server running on port", PORT);
