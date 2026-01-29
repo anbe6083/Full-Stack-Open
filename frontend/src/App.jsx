3fbe59682664325d26a1a90a5b10e4194aac7385
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2026-01-29 08:58:06 -0800

    Create production build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,75 +1,76 @@
-import { useState, useEffect } from 'react'
-import Footer from './components/Footer'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import noteService from './services/notes'
+import {useState, useEffect} from 'react';
+import Footer from './components/Footer';
+import Note from './components/Note';
+import Notification from './components/Notification';
+import noteService from './services/notes';
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [notes, setNotes] = useState ([]);
+  const [newNote, setNewNote] = useState ('');
+  const [showAll, setShowAll] = useState (true);
+  const [errorMessage, setErrorMessage] = useState (null);
 
-  useEffect(() => {
-    noteService.getAll().then(initialNotes => {
-      setNotes(initialNotes)
-    })
-  }, [])
+  useEffect (() => {
+    noteService.getAll ().then (initialNotes => {
+      setNotes (initialNotes);
+    });
+  }, []);
 
   const addNote = event => {
-    event.preventDefault()
+    event.preventDefault ();
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5
-    }
+      important: Math.random () > 0.5,
+    };
 
-    noteService.create(noteObject).then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNewNote('')
-    })
-  }
+    noteService.create (noteObject).then (returnedNote => {
+      setNotes (notes.concat (returnedNote));
+      setNewNote ('');
+    });
+  };
 
   const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
+    const note = notes.find (n => n.id === id);
+    const changedNote = {...note, important: !note.important};
 
     noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
+      .update (id, changedNote)
+      .then (returnedNote => {
+        setNotes (notes.map (note => (note.id !== id ? note : returnedNote)));
       })
-      .catch(error => {
-        setErrorMessage(
+      .catch (error => {
+        console.error ('Error updating note:', error);
+        setErrorMessage (
           `Note '${note.content}' was already removed from server`
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
+        );
+        setTimeout (() => {
+          setErrorMessage (null);
+        }, 5000);
+        setNotes (notes.filter (n => n.id !== id));
+      });
+  };
 
   const handleNoteChange = event => {
-    setNewNote(event.target.value)
-  }
+    setNewNote (event.target.value);
+  };
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter (note => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
+        <button onClick={() => setShowAll (!showAll)}>
           show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => (
+        {notesToShow.map (note => (
           <Note
             key={note.id}
             note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
+            toggleImportance={() => toggleImportanceOf (note.id)}
           />
         ))}
       </ul>
@@ -79,7 +80,7 @@ const App = () => {
       </form>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
