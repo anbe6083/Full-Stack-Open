2accd94da269c00b5a8836750644ffc1ed802de5
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2026-01-31 09:16:14 -0800

    Part 3: Connect backend to mongodb database and create note model

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
-const cors = require("cors");
+require("dotenv").config();
 const express = require("express");
+const Note = require("./models/note");
+
 const app = express();
 
 let notes = [
@@ -27,18 +29,15 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 const generateId = () => {
@@ -56,15 +55,13 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
-
-  notes = notes.concat(note);
-
-  response.json(note);
+  });
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -74,7 +71,7 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
