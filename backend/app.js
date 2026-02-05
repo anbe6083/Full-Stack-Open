06dcef0847d45c03899f0e06bc03653b47b5c305
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2026-02-04 16:55:54 -0800

    Add login and user routes and token authentication

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const app = express();
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 logger.info("connecting to", config.MONGODB_URI);
 mongoose
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,8 +1,18 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -17,7 +27,16 @@ notesRouter.get("/:id", async (request, response) => {
 
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const decodedToken = jwt.decode(getTokenFrom(request), process.env.SECRET);
+  if (decodedToken === null || !decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
 
+  const user = await User.findById(decodedToken.id);
+
+  if (!user) {
+    return response.status(400).json({ error: "userId missing or not valid" });
+  }
   if (!body.content) {
     return response.status(400).json({
       error: "content missing",
@@ -27,8 +46,12 @@ notesRouter.post("/", async (request, response, next) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id,
   });
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
